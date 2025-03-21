f00b985358cc1ea858e510eb9565cfbb2f53dd86
Raman Kumar Kheti <ramank.kheti@gmail.com>
2025-03-21 17:20:52 +0530

    Implemented rotate an array from a given value

---
+++ Array/rotate-an-array-counterclockwise.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include <cmath>
+
+using namespace std;
+
+
+void rotateArrayCounterClockwise(int *array, int size, int k) {
+
+    for (int i = 0; i < k; i++) {                // Move next until reach to the k
+
+            for (int j = 0; j < size -1 ; j++){ // Swipe the index to n -1 
+                    int tamp = array[j];          // Store the index value as tamp
+                    array[j] = array[j + 1];     // Assign the index + 1 value into index
+                    array[j + 1] = tamp;        // Assign tamp value into index + 1 
+            }
+    }
+}
+
+
+int main() {
+    // Test array
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16};
+    
+    int k = 6;
+    
+    int size = sizeof(arr) / sizeof(arr[0]); // Calculate size of array
+
+    // Call the function to reverse in groups
+    rotateArrayCounterClockwise(arr, size, k);
+
+    // Output the modified array
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
