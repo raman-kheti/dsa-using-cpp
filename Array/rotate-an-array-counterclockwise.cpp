8fba5714e0c0fd7f0a72f0065dfd2eb796c3ddb6
Raman Kumar Kheti <ramank.kheti@gmail.com>
2025-03-21 17:27:40 +0530

    Optimized the code

--- Array/rotate-an-array-counterclockwise.cpp
+++ Array/rotate-an-array-counterclockwise.cpp
@@ -6,13 +6,17 @@ using namespace std;
 
 void rotateArrayCounterClockwise(int *array, int size, int k) {
 
-    for (int i = 0; i < k; i++) {                // Move next until reach to the k
+    if(k > size-1)
+        return;
+    for (int i = 0; i < k; i++)
+    { // Move next until reach to the k
 
-            for (int j = 0; j < size -1 ; j++){ // Swipe the index to n -1 
-                    int tamp = array[j];          // Store the index value as tamp
-                    array[j] = array[j + 1];     // Assign the index + 1 value into index
-                    array[j + 1] = tamp;        // Assign tamp value into index + 1 
-            }
+        for (int j = 0; j < size - 1; j++)
+        {                            // Swipe the index to n -1           // Do not need to check grater number since we are not sorting just rotating the array
+            int tamp = array[j];     // Store the index value as tamp
+            array[j] = array[j + 1]; // Assign the index + 1 value into index
+            array[j + 1] = tamp;     // Assign tamp value into index + 1
+        }
     }
 }
 
@@ -21,7 +25,7 @@ int main() {
     // Test array
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16};
     
-    int k = 6;
+    int k = 15;
     
     int size = sizeof(arr) / sizeof(arr[0]); // Calculate size of array
 
